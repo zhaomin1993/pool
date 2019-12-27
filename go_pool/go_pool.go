63da01404c1e06f1dfa4ed3737e73b288906334e
赵元昊 <zm@nosugartech.com>
2019-12-27 18:14:41 +0800

    做人留一线，自动缩容时最小至1

--- go_pool/go_pool.go
+++ go_pool/go_pool.go
@@ -221,10 +221,7 @@ func (wp *workerPool) autoCutCap(interval time.Duration) {
 			case <-ticker.C:
 				length := len(wp.workerQueue)
 				wp.mux.RLock()
-				if uint16(length) == wp.aliveNum {
-					wp.mux.RUnlock()
-					wp.adjustNum(0)
-				} else if 1 < length && wp.workerNum > 1 {
+				if uint16(length) == wp.aliveNum && wp.workerNum != 0 {
 					wp.mux.RUnlock()
 					wp.adjustNum(uint16(length / 2))
 				} else {
