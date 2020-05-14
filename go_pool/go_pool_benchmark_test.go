7b730844cfc9959b4276e03cd14f0dc2d23084b4
赵元昊 <zm@nosugartech.com>
2020-05-14 17:51:42 +0800

    添加了benchmark test

---
+++ go_pool/go_pool_benchmark_test.go
@@ -0,0 +1,110 @@
+package go_pool
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+const (
+	RunTimes           = 1000000
+	BenchParam         = 10
+	BenchAntsSize      = 20000
+	DefaultExpiredTime = 10 * time.Second
+)
+
+type obj struct {}
+
+func (obj) Do() {
+	demoFunc()
+}
+
+func demoFunc() {
+	time.Sleep(time.Duration(BenchParam) * time.Millisecond)
+}
+
+//go test -bench=BenchmarkGoroutines -benchmem=true -run=none
+//BenchmarkGoroutines-8                          1        3276239400 ns/op        537844448 B/op   1997684 allocs/op
+func BenchmarkGoroutines(b *testing.B) {
+	var wg sync.WaitGroup
+	for i := 0; i < b.N; i++ {
+		wg.Add(RunTimes)
+		for j := 0; j < RunTimes; j++ {
+			go func() {
+				demoFunc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+//go test -bench=BenchmarkSemaphore -benchmem=true -run=none
+//BenchmarkSemaphore-8                           2         651259850 ns/op        64292344 B/op    1001274 allocs/op
+func BenchmarkSemaphore(b *testing.B) {
+	var wg sync.WaitGroup
+	sema := make(chan struct{}, BenchAntsSize)
+
+	for i := 0; i < b.N; i++ {
+		wg.Add(RunTimes)
+		for j := 0; j < RunTimes; j++ {
+			sema <- struct{}{}
+			go func() {
+				demoFunc()
+				<-sema
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+//go test -bench=BenchmarkGoPool -benchmem=true -run=none
+//BenchmarkGoPool-8                      2         645773800 ns/op         3646412 B/op      47589 allocs/op
+func BenchmarkGoPool(b *testing.B) {
+	p := NewWorkerPool(uint16(BenchAntsSize), uint16(BenchAntsSize), DefaultExpiredTime)
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < RunTimes; j++ {
+			_ = p.Accept(obj{})
+		}
+	}
+	p.Close()
+	b.StopTimer()
+}
+
+//BenchmarkGoroutinesThroughput-8                4         264044400 ns/op        63998256 B/op     999972 allocs/op
+func BenchmarkGoroutinesThroughput(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < RunTimes; j++ {
+			go demoFunc()
+		}
+	}
+}
+
+//BenchmarkSemaphoreThroughput-8                 2         631312300 ns/op        64034400 B/op    1000145 allocs/op
+func BenchmarkSemaphoreThroughput(b *testing.B) {
+	sema := make(chan struct{}, BenchAntsSize)
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < RunTimes; j++ {
+			sema <- struct{}{}
+			go func() {
+				demoFunc()
+				<-sema
+			}()
+		}
+	}
+}
+
+//BenchmarkGoPoolThroughput-8            2         643280650 ns/op         8380768 B/op      62874 allocs/op
+func BenchmarkGoPoolThroughput(b *testing.B) {
+	p := NewWorkerPool(uint16(BenchAntsSize), uint16(BenchAntsSize), DefaultExpiredTime)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < RunTimes; j++ {
+			_ = p.Accept(obj{})
+		}
+	}
+	b.StopTimer()
+}
