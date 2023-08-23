bc3fc3d7b2523d03e1d4189958afa4ba4b9ad4e5
melo <35480270+zhaomin1993@users.noreply.github.com>
2023-08-23 14:03:41 +0800

    Update pool.go

--- pool.go
+++ pool.go
@@ -3,6 +3,7 @@ package pool
 import "github.com/zhaomin1993/pool/internal"
 
 type Pool interface {
+	OnPanic(onPanic func(msg interface{}))
 	Accept(job internal.Job) (err error)
 	Len() uint16
 	AdjustSize(workNum uint16)
